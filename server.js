3575a933ce98cb55fc9afa6ecd7b3d4822dcb289
Vartika Singh <vartika8144@gmail.com>
2021-06-30 00:12:15 +0530

    First commit: Set up directory structure and create server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const app = express();
 
-	
+const port = process.env.PORT || 8000;
+
+app.listen(port, (err) => {
+	if(err){
+		console.log(`Error in running the server: ${err}`);
+		return;
+	}
+	console.log(`Server running on port: ${port}`);
+})
